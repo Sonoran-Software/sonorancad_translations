deb118ce31a0f1687f3902ab018bee37317310d3
Brian Sosnowski <56844125+SonoranBrian@users.noreply.github.com>
2024-04-23 18:53:34 +0000

    en update + new tr

---
+++ index.js
@@ -0,0 +1,32 @@
+import enUS from './en.json';
+import cn from './cn.json';
+import de from './de.json';
+import fr from './fr.json';
+import es from './es.json';
+import no from './no.json';
+import pl from './pl.json';
+import pt from './pt.json';
+import ar from './ar.json';
+import it from './it.json';
+import ru from './ru.json';
+import tn from './tc.json';
+import tr from './tr.json';
+import nlBe from './nl-be.json';
+
+/* eslint-disable quote-props */
+export default {
+  'en-US': enUS,
+  'cn': cn,
+  'de': de,
+  'es': es,
+  'fr': fr,
+  'it': it,
+  'no': no,
+  'pl': pl,
+  'pt': pt,
+  'ar': ar,
+  'ru': ru,
+  'tn': tn,
+  'tr': tr,
+  'nl-be': nlBe,
+};
