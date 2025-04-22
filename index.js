c2eed8354fa67ae5b0d12897ae64d8d7a137c6a3
Brian Sosnowski <sosnowskibrian@gmail.com>
2025-04-22 12:12:33 -0700

    Add UA

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import enUS from './en.json';
+import enUS from './en-US.json';
 import cn from './cn.json';
 import de from './de.json';
 import fr from './fr.json';
@@ -12,21 +12,23 @@ import ru from './ru.json';
 import tn from './tc.json';
 import tr from './tr.json';
 import nlBe from './nl-be.json';
+import ua from './ua.json';
 
 /* eslint-disable quote-props */
 export default {
-  'en-US': enUS,
-  'cn': cn,
-  'de': de,
-  'es': es,
-  'fr': fr,
-  'it': it,
-  'no': no,
-  'pl': pl,
-  'pt': pt,
-  'ar': ar,
-  'ru': ru,
-  'tn': tn,
-  'tr': tr,
-  'nl-be': nlBe,
+  'en-US': enUS, // English (United States)
+  'cn': cn, // Chinese (Simplified)
+  'de': de, // German
+  'es': es, // Spanish
+  'fr': fr, // French
+  'it': it, // Italian
+  'no': no, // Norwegian
+  'pl': pl, // Polish
+  'pt': pt, // Portuguese
+  'ar': ar, // Arabic
+  'ru': ru, // Russian
+  'tn': tn, // Chinese (Traditional)
+  'tr': tr, // Turkish
+  'ua': ua, // Ukrainian
+  'nl-be': nlBe, // Dutch (Belgium)
 };
