6736cce3d318d924f6c077732780a46275c11bff
Brian Sosnowski <56844125+SonoranBrian@users.noreply.github.com>
2026-05-13 11:51:59 -0700

    chore: update i18n source strings and languages

--- index.js
+++ index.js
@@ -1,36 +1,59 @@
 import enUS from './en.json';
+import cs from './cs.json';
 import cn from './zh-CN.json';
+import da from './da.json';
 import de from './de.json';
 import fr from './fr.json';
 import es from './es.json';
+import esEs from './es-ES.json';
+import fi from './fi.json';
 import no from './no.json';
 import pl from './pl.json';
 import pt from './pt.json';
 import ar from './ar.json';
 import it from './it.json';
+import ja from './ja.json';
 import ru from './ru.json';
 import tn from './zh-TW.json';
 import tr from './tr.json';
 import nlBe from './nl-BE.json';
+import ko from './ko.json';
 import ua from './uk.json';
 import koKR from './ko-KR.json';
+import nl from './nl.json';
+import sv from './sv.json';
+import zh from './zh.json';
 
 /* eslint-disable quote-props */
 export default {
+  'ar': ar, // Arabic
+  'cs': cs, // Czech
+  'da': da, // Danish
+  'de': de, // German
+  'en': enUS, // English
   'en-US': enUS, // English (United States)
   'cn': cn, // Chinese (Simplified)
-  'de': de, // German
   'es': es, // Spanish
+  'es-ES': esEs, // Spanish (Spain)
+  'fi': fi, // Finnish
   'fr': fr, // French
   'it': it, // Italian
+  'ja': ja, // Japanese
+  'ko': ko, // Korean
   'no': no, // Norwegian
   'pl': pl, // Polish
   'pt': pt, // Portuguese
-  'ar': ar, // Arabic
   'ru': ru, // Russian
+  'sv': sv, // Swedish
   'tn': tn, // Chinese (Traditional)
   'tr': tr, // Turkish
   'ua': ua, // Ukrainian
+  'uk': ua, // Ukrainian
+  'nl': nl, // Dutch
   'nl-be': nlBe, // Dutch (Belgium)
+  'nl-BE': nlBe, // Dutch (Belgium)
   'ko-KR': koKR, // Korean
+  'zh': zh, // Chinese
+  'zh-CN': cn, // Chinese (Simplified)
+  'zh-TW': tn, // Chinese (Traditional)
 };
